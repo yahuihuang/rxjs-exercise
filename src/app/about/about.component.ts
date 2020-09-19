713fbd42ee4cf546d20aefaa5926cc374e7f3d41
huang.yahui.grace <huang.yahui.grace@gmail.com>
2020-09-19 11:51:25 +0800

    concat

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -77,5 +77,12 @@ export class AboutComponent implements OnInit {
       //   }
       // );
 
+      // concat
+      const source1$ = of([1, 2, 3]);
+      const source2$ = of([4, 5, 6]);
+      const source3$ = of([7, 8, 9]);
+
+      const combine$ = concat(source1$, source2$, source3$);
+      combine$.subscribe(console.log);
     }
 }
