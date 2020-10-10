9bb368240b55f86c24a3fcc99aa73f233c4bd080
huang.yahui.grace <huang.yahui.grace@gmail.com>
2020-10-10 23:21:10 +0800

    category

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { StoreService } from './common/store.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+
+  constructor(private store: StoreService) {
+
+  }
+
+  ngOnInit(): void {
+    this.store.init();
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,7 @@ import {Course} from "../model/course";
 import {interval, noop, Observable, of, throwError, timer} from 'rxjs';
 import {catchError, delay, delayWhen, finalize, map, retryWhen, shareReplay, tap} from 'rxjs/operators';
 import {createHttpObservable} from '../common/util';
+import { StoreService } from 'app/common/store.service';
 
 
 @Component({
@@ -16,34 +17,13 @@ export class HomeComponent implements OnInit {
 
     advancedCourses$: Observable<Course[]>;
 
-    ngOnInit() {
-
-        const http$ = createHttpObservable('/api/courses');
-
-        const courses$: Observable<Course[]> = http$
-            .pipe(
-                tap(() => console.log("HTTP request executed")),
-                map(res => Object.values(res["payload"]) ),
-                shareReplay(),
-                retryWhen(errors =>
-                    errors.pipe(
-                    delayWhen(() => timer(2000)
-                    )
-                ) )
-            );
-
-        this.beginnerCourses$ = courses$
-            .pipe(
-                map(courses => courses
-                    .filter(course => course.category == 'BEGINNER'))
-            );
-
-        this.advancedCourses$ = courses$
-            .pipe(
-                map(courses => courses
-                    .filter(course => course.category == 'ADVANCED'))
-            );
+    constructor(private store: StoreService) {
 
     }
 
+    ngOnInit() {
+        const courses$ = this.store.courses$;
+        this.beginnerCourses$ = this.store.selectBeginnerCourses();
+        this.advancedCourses$ = this.store.selectAdvancedCourses();
+    }
 }
